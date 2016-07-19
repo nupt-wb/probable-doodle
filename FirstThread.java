0593ddf4ec5fea169ecafd2c7b16a741b251ed8d
nupt-wb <1107474476@qq.com>
2016-07-19 13:22:27 +0800

    Add files via upload

---
+++ FirstThread.java
@@ -0,0 +1,18 @@
+
+public class FirstThread extends Thread {
+	private int i;
+	public void run(){
+		for (; i < 1000; i++){
+			System.out.println(getName()+" "+i);
+		}
+	}
+	public static void main(String args[]){
+		for(int i = 0; i < 100; i++){
+			System.out.println(Thread.currentThread().getName()+" "+i);
+			if (i == 20){
+				new FirstThread().start();
+				new FirstThread().start();
+			}
+		}
+	} 
+}
